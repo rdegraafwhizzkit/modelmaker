3d83957b82bfef15f9260e547d8d13652d92fe42
Rutger de Graaf <rutger@whizzkit.nl>
2024-05-23 16:32:31 +0200

    Added logging and more configuration

--- inference.py
+++ inference.py
@@ -1,42 +1,51 @@
 from PIL import Image
 from os.path import expanduser
-from pathlib import Path
 from pycoral.adapters import common, classify
 import tflite_runtime.interpreter as tflite
 from sys import platform
 from ctypes.util import find_library
 from dynaconf import Dynaconf
+from logging import info, warning, basicConfig, INFO
 
+basicConfig(level=INFO)
 settings = Dynaconf(settings_files=['settings.yaml'], environments=True)
 
 library = settings.libraries.get(platform)
-script_dir = Path(__file__).parent.absolute()
 
 experimental_delegates = []
-model_path = f'{script_dir}/models/model.tflite'
-if find_library(library) and settings.use_edge_tpu:
+model_path = settings.model.regular
+if find_library(library) and settings.use_edgetpu:
     try:
+        info('Trying to load EdgeTPU libraries')
         experimental_delegates = [tflite.load_delegate(library)]
-        model_path = f'{script_dir}/models/model_edgetpu.tflite'
+        model_path = settings.model.edgetpu
+        info('EdgeTPU libraries loaded')
     except ValueError:
-        print('EdgeTPU hardware not found, using CPU')
+        warning('EdgeTPU hardware not found, using CPU')
+else:
+    info('Configured not to use EdgeTPU' if not settings.use_edgetpu else 'EdgeTPU library not found')
 
-image_file = f'{expanduser("~")}/.keras/datasets/flower_photos/daisy/102841525_bd6628ae3c.jpg'
-
-labels_path = f'{script_dir}/models/labels.txt'
+labels_path = settings.model.labels
 labels = {i: label.strip() for i, label in enumerate(open(labels_path, 'r').readlines())}
+info(f'Loaded {len(labels)} labels')
 
+info(f'Creating interpreter')
 interpreter = tflite.Interpreter(
     model_path,
     experimental_delegates=experimental_delegates
 )
-
 interpreter.allocate_tensors()
-size = common.input_size(interpreter)
 
+info(f'Preparing input image')
+size = common.input_size(interpreter)
+image_file = f'{expanduser("~")}/.keras/datasets/flower_photos/daisy/102841525_bd6628ae3c.jpg'
 image = Image.open(image_file).convert('RGB').resize(size, Image.Resampling.LANCZOS)
+
+info(f'Inferencing')
 common.set_input(interpreter, image)
 interpreter.invoke()
+
+info(f'Detected objects are:')
 classes = classify.get_classes(interpreter, top_k=1)
 for i, clazz in enumerate(classes):
-    print(f'{i}: {labels.get(clazz.id)} / {clazz.score}')
+    info(f'id: {i}, label: {labels.get(clazz.id)}, score: {clazz.score}')
